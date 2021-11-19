b957a8085d9f289926d0faf298c92342d763b825
mihir jayavant <mihir.jayavant2@gmail.com>
2021-11-19 23:37:14 +0530

    clear clutter

--- CommandLineCalendar/Program.cs
+++ CommandLineCalendar/Program.cs
@@ -47,36 +47,9 @@ do
 
 } while (option == 0);
 
-DateTime startingMessage()
+static DateTime startingMessage()
 {
     var d = DateTime.Now;
     Console.WriteLine("Welcome to Command Line Calender\nToday's date:" + d + "\nFor help enter: help");
     return d;
-}
-
-void Show(CalenderInteraction c, int month)
-{
-    int temp = c.month;
-    c.month = month;
-    Console.WriteLine("\t\t\t" + CalenderInteraction.getSpecificMonth(month).ToUpper() + " " + c.getYear());
-    Console.WriteLine("\t" + c.getAllDayInName() + "\n" +
-                      CalenderInteraction.formatToCalendar(c.getCalendarOfMonth()));
-    c.month = temp;
-}
-
-bool CheckIfNum(string s)
-{
-    bool b = false;
-    for (int i = 0; i < s.Length; i++)
-    {
-        if (char.IsDigit(s[i]))
-        {
-            continue;
-        }
-        else
-        {
-            return b;
-        }
-    }
-    return true;
 }
